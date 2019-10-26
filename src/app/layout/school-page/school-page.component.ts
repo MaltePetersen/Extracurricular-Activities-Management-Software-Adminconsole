c55bfb385ad4d11107bfb1dadd9a3978a57a8b06
MaltePetersen <petersen-malte@web.de>
2019-10-26 13:30:26 +0200

    Patch is working with schools now

--- src/app/layout/school-page/school-page.component.ts
+++ src/app/layout/school-page/school-page.component.ts
@@ -4,6 +4,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { School } from 'src/app/model/School.model';
 import { SchoolDTO } from 'src/app/model/SchoolDTO.model';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'app-school-page',
@@ -14,9 +15,12 @@ import { SchoolDTO } from 'src/app/model/SchoolDTO.model';
 
 })
 export class SchoolPageComponent implements OnInit {
-    constructor(public http: HttpClient) { }
+    constructor(public http: HttpClient, private modalService: NgbModal) { }
     schools: School[];
     model: SchoolDTO = new SchoolDTO('', '', '', '');
+    closeResult: string;
+    modalSchool: SchoolDTO = new SchoolDTO('','','','');
+
     ngOnInit() {
         this.getAllSchools();
     }
@@ -32,5 +36,22 @@ export class SchoolPageComponent implements OnInit {
     patchSchoolById(id: number, school: School){
         this.http.patch<School>(`${environment.apiUrl}/api/schools/${id}`,school).subscribe(() => this.getAllSchools());
     }
-
+    open(school: School,content) {
+        this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+          this.closeResult = `Closed with: ${result}`;
+          this.patchSchoolById(school.id,new School(school.id, this.modalSchool.name,this.modalSchool.address,this.modalSchool.email, this.modalSchool.phoneNumber));
+          this.modalSchool = new SchoolDTO('','','','');
+        }, (reason) => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        });
+      }
+    private getDismissReason(reason: any): string {
+        if (reason === ModalDismissReasons.ESC) {
+          return 'by pressing ESC';
+        } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+          return 'by clicking on a backdrop';
+        } else {
+          return  `with: ${reason}`;
+        }
+      }
 }
--- src/app/layout/school-page/school-page.module.ts
+++ src/app/layout/school-page/school-page.module.ts
@@ -4,9 +4,10 @@ import { PageHeaderModule } from 'src/app/shared';
 import { SchoolPageComponent } from './school-page.component';
 import { SchoolPageRoutingModule } from './school-page-routing.module';
 import { FormsModule } from '@angular/forms';
+import {  NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-    imports: [CommonModule, PageHeaderModule, FormsModule,  SchoolPageRoutingModule],
+    imports: [CommonModule, PageHeaderModule,NgbModule, FormsModule,  SchoolPageRoutingModule],
     declarations: [SchoolPageComponent]
 })
 export class SchoolPageModule {}
